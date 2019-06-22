b10df54fdbcdbe527a7b2d4fdba65a9c71d5e882
Zhu Xi <zhuxi910511@163.com>
2019-06-23 01:28:39 +0800

    ss and privoxy

---
+++ run_docker.sh
@@ -0,0 +1,3 @@
+docker run -d --restart=unless-stopped --name=ss-local -v /root/conf/ss-local.json:/config.json -p 1080:1080 shadowsocks/shadowsocks-libev ss-local -c /config.json
+
+docker run -d --restart unless-stopped --name privoxy -v /root/conf/privoxy.conf:/etc/privoxy/config -p 8118:8118 splazit/privoxy-alpine
